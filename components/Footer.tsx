9c9e88cd50a9409f702eea8023482fdaa504a305
gf-co <145439449+gf-co@users.noreply.github.com>
2023-10-28 01:11:53 -0700

    add contact links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,32 @@ import { Link } from "@nextui-org/link";
 export default function Footer() {
   return (
     <div className="mt-32 flex flex-col gap-10">
-      <Link isExternal href="" underline="hover" color="foreground">
-        Source Code
-      </Link>
+      <div className="flex flex-wrap gap-5">
+        <Link
+          isExternal
+          href="https://github.com/gf-co/nextjs-with-supabase"
+          underline="hover"
+          color="foreground"
+        >
+          Source Code
+        </Link>
+        <Link
+          isExternal
+          href="https://gf-co.github.io/"
+          underline="hover"
+          color="foreground"
+        >
+          Portfolio
+        </Link>
+        <Link
+          isExternal
+          href="https://www.linkedin.com/in/gf-co/"
+          underline="hover"
+          color="foreground"
+        >
+          LinkedIn
+        </Link>
+      </div>
       <small className="text-sm opacity-50">
         Built with Next.js, Supabase, NextUI, and Tailwind CSS
       </small>
